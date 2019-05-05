b6841afa516c29b7b9c307cd530e9d1cf99de152
EC2 Default User <ec2-user@ip-172-31-1-144.us-east-2.compute.internal>
2019-05-05 11:58:30 +0000

    up to production

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
